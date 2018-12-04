5fd5842a0b8a50c190cd42f5c8ec2958c39f8fa6
Daniel Aron Davíðsson <danield@ru.isgit config --global user.name danield1818>
2018-12-04 13:26:09 +0000

    litlar breytingar

---
+++ services/login.py
@@ -0,0 +1,10 @@
+from .repo.ReadUserCsv import ReadUserCsv
+user_list = ReadUserCsv.readData()
+print(user_list)
+#class Login():
+    # Send csv to helper to get only user_name/password/admin
+    #def getUserAndPassword():
+
+    # kalla í 2 önnur föll "user exits" og "password match (correct password)"
+    #def validateUser():
+        
