af93d43a524cf98e8967691b8a4af4113b049b34
Daniel Aron Davíðsson <danield@ru.isgit config --global user.name danield1818>
2018-12-07 10:36:40 +0000

    smávægis breytingar til að fá customer til að virka í ui

--- ui/CustMenu/CustMenu.py
+++ ui/CustMenu/CustMenu.py
@@ -1,16 +1,18 @@
 import os
+from services.Customer import Customer
 
 class CustMenu:
-    def __init__():
-        pass
-    def customerMenu(self, check_if_admin):
+    def __init__(self, manager):
+        self.__manager = manager
+        self.__customer = Customer()
+
+    def customerMenu(self):
         selection = ""
         os.system('cls')
         while (selection !="9"):
             print("Customers")
             print("1. Lookup Customer")
-            if check_if_admin:
-                print("2. Register Customer")
+            print("2. Register Customer")
             print("9. Go Back")
             print("--------------")
             selection = input()
@@ -19,9 +21,10 @@ class CustMenu:
             if selection == "1":
                 customer = self.__customer.lookUpCustomer()
                 if customer != None:
-                    Interface.customerMenuSelection(self, check_if_admin, customer)
+                    self.__manager.gotoclass("lookupcustomermenu")
+                    #LookupCustomerMenu(self, customer)
                 else:
                     print("Customer does not exist")                   
-            elif selection == "2" and check_if_admin:
+            elif selection == "2":
                 self.__customer.registerCustomer()
                 os.system('cls')
