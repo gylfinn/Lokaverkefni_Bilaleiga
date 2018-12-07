aee401ad95418de8914bf4dcbef130d67265e7b3
Gylfi Helgason <gylfisuf@gmail.com>
2018-12-07 14:02:08 +0000

    Setti inn CarMenu.py

---
+++ ui/CarMenu/CarMenu.py
@@ -0,0 +1,22 @@
+import os
+
+#This class is only 
+class CarMenu:
+    def __init__(self, manager):
+        self.__manager = manager
+    def carMenu(self):
+        os.system('cls')
+        car_menu_selection = ""
+        while(car_menu_selection !="9"):
+            print("Car Menu")
+            print("1. Car Administration")
+            print("2. Fleet Manager")
+            print("9. Back")
+            car_menu_selection = input()
+            os.system('cls')
+            if car_menu_selection == "1":
+                self.__manager.gotoClass("caradministration")
+            elif car_menu_selection == "2":
+                self.__manager.gotoClass("fleetmanager")
+            elif car_menu_selection =="9":
+                self.__manager.gotoClass("mainmenu")
