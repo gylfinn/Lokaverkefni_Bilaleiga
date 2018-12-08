c3f7fde81ef3912351bbfd0b6dae4f8b0cc3619c
LampCat <vikinguraron@gmail.com>
2018-12-08 17:24:58 +0000

    Removed usless classes in Models

--- Models/SUV.py
+++
@@ -1,5 +0,0 @@
-from Vehicle import Vehicle
-
-class SUV(Vehicle):
-    def __init__(self, registration_num, model_year, brand, price):
-        super().__init__(registration_num, model_year, brand, price)
--- Models/Sedan.py
+++
@@ -1,5 +0,0 @@
-from Vehicle import Vehicle
-
-class Sedan(Vehicle):
-    def __init__(self, registration_num, model_year, brand, price):
-        super().__init__(registration_num, model_year, brand, price)
