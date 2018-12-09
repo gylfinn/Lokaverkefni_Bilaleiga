fc51b1f3abd289913ac90e1a3ab4f1a9564437db
Daniel Aron Davíðsson <danield@ru.isgit config --global user.name danield1818>
2018-12-09 23:28:12 +0000

    lagað smávægis stafsetningavillur í getorderdata

--- repo/GetOrderData.py
+++ repo/GetOrderData.py
@@ -4,7 +4,7 @@ class GetOrderData:
     def __init__(self):
         self.__order_data = []
 
-    def readCustomerData(self):
+    def readOrderData(self):
         with open ("./data/orders.csv", "r") as order_file:
             csv_reader = csv.reader(order_file)
             next(csv_reader)
@@ -17,7 +17,7 @@ class GetOrderData:
             order_writer = csv.writer(order_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             order_writer.writerow([orderid, carregistrationnumber, customerssn, datefrom, dateto, totalprice])
 
-    def changeCustomerInfo(self, order, name, replace_index):
+    def changeOrderInfo(self, order, name, replace_index):
         new_list = []
         with open("./data/orders.csv", "r") as order_file_r:
             reader = csv.reader(order_file_r)
@@ -35,7 +35,7 @@ class GetOrderData:
             for line in new_list:
                 writer.writerow(line)
 
-    def deleteCustomer(self, order, orderid):
+    def deleteOrder(self, order, orderid):
         new_list = []
         with open("./data/orders.csv", "r") as order_file_r:
             reader = csv.reader(order_file_r)
