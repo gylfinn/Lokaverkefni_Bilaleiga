e998af07db21605ea83ab9f4315ff203db57bd47
Daniel Aron Davíðsson <danield@ru.isgit config --global user.name danield1818>
2018-12-11 16:33:51 +0000

    fallegir litir

--- ui/MainMenu.py
+++ ui/MainMenu.py
@@ -1,4 +1,5 @@
 import os
+from colorama import Fore
 
 class MainMenu:
     def __init__(self, manager):
@@ -8,10 +9,13 @@ class MainMenu:
         selection = ""
         while(selection !="9"):
             print("Main Menu")
+            print(Fore.GREEN,end="")
             print("1. Order")
             print("2. Cars")
             print("3. Customers")
+            print(Fore.RED,end="")
             print("9. Log out")
+            print(Fore.WHITE,end="")
             print("--------------")
             selection = input()
             os.system('cls')
