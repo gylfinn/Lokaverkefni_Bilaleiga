11ceba986bd2b8ebabb7470f297e9d439ffe59e3
Daniel Aron Davíðsson <danield@ru.isgit config --global user.name danield1818>
2018-12-11 16:09:03 +0000

    fallegir litir

--- ui/OrderMenu/UpcomingOrders/UpcomingOrders.py
+++ ui/OrderMenu/UpcomingOrders/UpcomingOrders.py
@@ -1,4 +1,5 @@
 import os
+from colorama import Fore
 
 #This class calls a service class that returns this class a list of upcoming orders
 #This class then displays that to the user
@@ -6,13 +7,16 @@ import os
 class UpcomingOrders:
     def __init__(self, manager):
         self.__manager = manager
+
     def upcomingOrders(self):
         os.system('cls')
         upcoming_order_menu_selection = ""
         while(upcoming_order_menu_selection !="9"):
             Orders = self.__manager.getOrderManager().getOrders(True)
             for order in Orders:
+                print(Fore.YELLOW,end="")
                 print(order)
+                print(Fore.WHITE,end="")
             print("9. Back")
             upcoming_order_menu_selection = input()
             if upcoming_order_menu_selection == "9":
